a15bc8f633e7bee3d877f8884e44766169ebaa61
Ljime <Ljime@users.noreply.github.com>
2021-08-23 22:41:43 -0400

    hi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-const stripePrivateKey = process.env.SECRET_STRIPE_KEY
+const stripePrivateKey =
+	"sk_test_51JQEWDBXBNrU1eQLIFso90NODhW1nkMTlL8DhcttmIFSkPYHOqVttj7kMphJHPAHWtANL77J9AwvHp13qrW0FWN500Yg00h9UP"
+// process.env.SECRET_STRIPE_KEY
 
 const express = require('express')
 const stripe = require('stripe')(stripePrivateKey)
