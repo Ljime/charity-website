7cfa006486299542c37a63f08026803ad4989b04
Ljime <Ljime@users.noreply.github.com>
2021-08-23 21:57:08 -0400

    production fix

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+require('dotenv').config()
 const stripePrivateKey = process.env.SECRET_STRIPE_KEY
 
 const express = require('express')
